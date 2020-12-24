e2e1388aee4c17677b48247e48ec45d8fc5f261f
vsawce <vincent.saw@sjsu.edu>
2020-12-24 00:18:30 -0800

    Modified spi driver to utilize conf.h. F_CPU is included in conf.h and is the first include directive

--- drivers/spi.c
+++ drivers/spi.c
@@ -1,33 +1,19 @@
 #include "spi.h"
 
-/* ARDUINO PRO MINI PINOUT:
- * CS:   PB2 (pin 10)
- * SCK:  PB5 (pin 13)
- * MOSI: PB3 (pin 11)
- * MISO: PB4 (pin 12)
- */
-
 void spi_init(void) {
-	//PORTD=0x00; //Set port D to LOW
-	//DDRD=0xFF; //Set port D to OUTPUT
 	
-	//MOSI, SCK, CS all high, output in master mode
-	//DDRB = (1<<MOSI) | (1<<CS) | (1<<SCK);
-	//PORTB = (1<<MOSI) | (1<<CS) | (1<<SCK);
+	/* MOSI, SCK, CS output in master mode */
 	DDRB = (1<<MOSI) | (1<<CS) | (1<<SCK);
-	//PORTB = (1<<MOSI) | (1<<SCK);
 	//Definitions defined in main.c
 	
-	//Set SPI Control Register bits, further explained below
-	//SPCR = (1<<MSTR) | (1<<SPR0);
-	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0); //ENABLES SPI
-	/*
-		SPE: SPI Enable bit, 1 = SPI enabled
-		MSTR: Select Master bit, 1 = master mode
-		SPR0: SPI Clock Rate 0 Select bit, SPR1 0 & SPR0 1 = SCK freq of f_osc/16
-			- Sets SCK freq to f_osc/16 setting, which matches MCU clock of 16MHz
-		CPOL & CPHA default to 0 so SPI mode is 0, which SDC uses
-	*/
+	/* Set SPI Control Register bits, further explained below */
+	//SPCR = (1<<MSTR) | (1<<SPR0); //SPI DISABLED BY DEFAULT
+	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0); //SPI ENABLED BY DEFAULT
+	/*	SPE: SPI Enable bit, 1 = SPI enabled									  
+	 *	MSTR: Select Master bit, 1 = master mode								  
+	 *	SPR0: SPI Clock Rate 0 Select bit, SPR1 0 & SPR0 1 = SCK freq of f_osc/16 
+	 *		- Sets SCK freq to f_osc/16 setting, which matches MCU clock of 16MHz
+	 *	CPOL & CPHA default to 0 so SPI mode is 0, which SDC uses				  */
 	
 }
 
@@ -51,12 +37,12 @@ void spi_write(unsigned char data) {
 unsigned char spi_receive(void) {
 	//Wait for data transmission to finish
 	while (!(SPSR & (1<<SPIF)));
-	
+	//Return received data, stored in SPDR register
 	return SPDR;
 }
 
 unsigned char spi_tranceiver (unsigned char data)
-{
+{	
 	SPDR = data;                                  //Load data into buffer
 	while(!(SPSR & (1<<SPIF) ));                  //Wait until transmission complete
 	return(SPDR);                                 //Return received data
--- drivers/spi.h
+++ drivers/spi.h
@@ -1,37 +1,18 @@
 #ifndef SPI_H
 #define SPI_H
 
-#define F_CPU 16000000UL //Change MCU speed here, delay.h dependent
+//#define F_CPU 16000000UL //Defined in conf.h
 
+#include "conf.h"
 #include <avr/io.h>
 #include <util/delay.h>
-#include "1602lcd.h"
 
-#define LED  2
-
-#define CS	 2
-#define MOSI 3
-#define MISO 4
-#define SCK  5
-
-/* ARDUINO PRO MINI PINOUT:
- * CS:   PB2 (pin 10)
- * SCK:  PB5 (pin 13)
- * MOSI: PB3 (pin 11)
- * MISO: PB4 (pin 12)
- */
-
-void spi_init(void);
-void spi_start(void);
-void spi_stop(void);
-void spi_write(unsigned char data);
-unsigned char spi_receive(void);
-unsigned char spi_tranceiver (unsigned char data);
-
-
-
-
-
+void spi_init(void); //Initialize dedicated SPI I/O
+void spi_start(void); //Start SPI
+void spi_stop(void); //Stop SPI
+void spi_write(unsigned char data); //Write (arg) data via SPDR register
+unsigned char spi_receive(void); //Receive (return) data via SPDR register
+unsigned char spi_tranceiver (unsigned char data); //Write (arg) and receive (return) data via SPDR register
 
 
 #endif /* SPI_H */
