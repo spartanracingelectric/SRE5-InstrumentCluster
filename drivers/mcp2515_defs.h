b88ef6dd7c7d0fa4522888c3a071f48a6970d97e
Sean Lin <nolinyc@gmail.com>
2021-03-13 22:49:41 -0800

    Rewrite mcp2515 driver

---
+++ drivers/mcp2515_defs.h
@@ -0,0 +1,181 @@
+/*
+ * mcp2515_defs.h
+ *
+ * 
+ *  Author: sean
+ */ 
+
+// mcp2515 defs contains register map of mcp2515
+
+#pragma once
+
+// SPI instruction set
+
+#define SPI_RESET				0xC0
+#define	SPI_READ				0x03
+#define	SPI_READ_RX_BUFFER		0x90
+#define	SPI_WRITE				0x02
+#define	SPI_LOAD_TX_BUFFER		0x40
+#define	SPI_RTS					0x80
+#define SPI_READ_STATUS			0xA0
+#define	SPI_RX_STATUS			0xB0
+#define	SPI_BIT_MODIFY			0x05
+
+// Control register summary
+
+#define BFPCTRL		0x0C
+#define TXRTSCTRL	0x0D
+#define CANSTAT		0x0E
+#define CANCTRL		0x0F
+#define TEC			0x1C
+#define REC         0x1D
+
+// Transmit buffer and control register 0 
+
+#define TXB0CTRL	0x30
+#define TXB0SIDH	0x31
+#define TXB0SIDL	0x32
+#define TXB0EID8	0x33
+#define TXB0EID0	0x34
+#define TXB0DLC		0x35
+#define TXB0D0		0x36
+#define TXB0D1		0x37
+#define TXB0D2		0x38
+#define TXB0D3		0x39
+#define TXB0D4		0x3A
+#define TXB0D5		0x3B
+#define TXB0D6		0x3C
+#define TXB0D7		0x3D
+
+// Transmit buffer and control register 1
+
+#define TXB1CTRL	0x40
+#define TXB1SIDH	0x41
+#define TXB1SIDL	0x42
+#define TXB1EID8	0x43
+#define TXB1EID0	0x44
+#define TXB1DLC		0x45
+#define TXB1D0		0x46
+#define TXB1D1		0x47
+#define TXB1D2		0x48
+#define TXB1D3		0x49
+#define TXB1D4		0x4A
+#define TXB1D5		0x4B
+#define TXB1D6		0x4C
+#define TXB1D7		0x4D
+
+// Transmit buffer and control register 2
+
+#define TXB2CTRL	0x50
+#define TXB2SIDH	0x51
+#define TXB2SIDL	0x52
+#define TXB2EID8	0x53
+#define TXB2EID0	0x54
+#define TXB2DLC		0x55
+#define TXB2D0		0x56
+#define TXB2D1		0x57
+#define TXB2D2		0x58
+#define TXB2D3		0x59
+#define TXB2D4		0x5A
+#define TXB2D5		0x5B
+#define TXB2D6		0x5C
+#define TXB2D7		0x5D
+
+// Receive buffer and control register 0 
+
+#define RXB0CTRL	0x60
+#define RXB0SIDH	0x61
+#define RXB0SIDL	0x62
+#define RXB0EID8	0x63
+#define RXB0EID0	0x64
+#define RXB0DLC		0x65
+#define RXB0D0		0x66
+#define RXB0D1		0x67
+#define RXB0D2		0x68
+#define RXB0D3		0x69
+#define RXB0D4		0x6A
+#define RXB0D5		0x6B
+#define RXB0D6		0x6C
+#define RXB0D7		0x6D
+
+// Receive buffer and control register 1
+
+#define RXB1CTRL	0x70
+#define RXB1SIDH	0x71
+#define RXB1SIDL	0x72
+#define RXB1EID8	0x73
+#define RXB1EID0	0x74
+#define RXB1DLC		0x75
+#define RXB1D0		0x76
+#define RXB1D1		0x77
+#define RXB1D2		0x78
+#define RXB1D3		0x79
+#define RXB1D4		0x7A
+#define RXB1D5		0x7B
+#define RXB1D6		0x7C
+#define RXB1D7		0x7D
+
+// Bit timing configuration register 
+
+#define CNF1		0x2A
+#define CNF2		0x29
+#define CNF3		0x28
+
+
+// RXBnCTRL register bit definitions
+
+#define RXM1		6
+#define RXM0		5
+#define RXRTR		3
+#define BUKT		2
+#define BUKT1		1
+#define FILHIT0		0
+
+// CAN control register bit definitions
+
+#define REQOP2		7
+#define REQOP1		6
+#define REQOP0		5
+#define ABAT		4
+#define CLKEN		2
+#define CLKPRE1		1
+#define CLKPRE0		0
+
+// CAN status register bit definitions
+
+#define OPMOD2		7
+#define OPMOD1		6
+#define OPMOD0		5
+#define ICOD2		3
+#define ICOD1		2
+#define ICOD0		1
+
+// CAN interrupt enable register
+
+#define CANINTE		0x2B
+
+// CAN interrupt enable register bit definitions
+
+#define MERRE		7
+#define WAKIE		6
+#define ERRIE		5
+#define TX2IE		4
+#define TX1IE		3
+#define TX0IE		2
+#define RX1IE		1
+#define RX0IE		0
+
+// CAN interrupt flag register 
+
+#define CANINTF		0x2C
+
+// CAN interrupt flag register bit definitions
+
+#define MERRF		7
+#define WAKIF		6
+#define ERRIF		5
+#define TX2IF		4
+#define TX1IF		3
+#define TX0IF		2
+#define RX1IF		1
+#define RX0IF		0
