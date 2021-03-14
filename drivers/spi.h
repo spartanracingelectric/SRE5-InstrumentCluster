8fae2847605976e1db5c44c5937549a0adcbddaa
Sean Lin <nolinyc@gmail.com>
2021-03-13 20:59:57 -0800

    Rewrite SPI driver

--- drivers/spi.h
+++ drivers/spi.h
@@ -1,18 +1,13 @@
-#ifndef SPI_H
-#define SPI_H
+/*
+ *  spi.h
+ *
+ * 
+ *  Author: sean
+ */ 
+#pragma once
 
-//#define F_CPU 16000000UL //Defined in conf.h
+#include <stdint.h>
 
-#include "conf.h"
-#include <avr/io.h>
-#include <util/delay.h>
+void spi_init(void); 
 
-void spi_init(void); //Initialize dedicated SPI I/O
-void spi_start(void); //Start SPI
-void spi_stop(void); //Stop SPI
-void spi_write(unsigned char data); //Write (arg) data via SPDR register
-unsigned char spi_receive(void); //Receive (return) data via SPDR register
-unsigned char spi_tranceiver (unsigned char data); //Write (arg) and receive (return) data via SPDR register
-
-
-#endif /* SPI_H */
+uint8_t spi_exchange_byte(uint8_t byte_to_transmit);
